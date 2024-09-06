0460cf5f010c8ed488c3809ca029591aa984891d
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-09-06 14:20:04 -0300

    separe el manejo de stock, pero todavia no solucione la actualizacion de stock en tiempo real

--- front/src/components/pages/Producto/ActualizarStock.jsx
+++ front/src/components/pages/Producto/ActualizarStock.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../../styles/actualizarStock.css";
 import handleStartScan from "../../../Hooks/Scan";
-import StockControl from "./StockControl";
 import { EgresarStock } from "./Stock/EgresarStock";
 import { IngresarStock } from "./Stock/IngresarStock";
+import ManejoStock from "./Stock/ManejoStock";
 
 export const ActualizarStock = () => {
   const navigate = useNavigate();
@@ -13,6 +13,7 @@ export const ActualizarStock = () => {
   const [accion, setAccion] = useState("");
   const [cantidad, setCantidad] = useState("");
   const scannerRef = useRef(null);
+  const [banderaStock, setBanderaStock] = useState(true);
 
   const handleAtras = () => {
     navigate("/");
@@ -27,10 +28,10 @@ export const ActualizarStock = () => {
     };
   }, []);
 
+
   const handleIniciarEscaneo = () => {
     handleStartScan(setScanResult, setSaborSeleccionado, scannerRef);
   };
-  
 
   const handleActualizarStock = () => {
     const cantidadNumero = parseInt(cantidad, 10);
@@ -54,37 +55,31 @@ export const ActualizarStock = () => {
         saborSeleccionado.id
       );
     }
-    handleAtras();
+    setBanderaStock(!banderaStock);
+  
   };
 
   return (
     <div className="container-actualizarStock">
       <div className="container-subb">
-          <button className = "boton-atras"onClick={handleAtras}>Atras</button>
-          <button className = "boton-escanear" onClick={handleIniciarEscaneo}>Iniciar escaneo</button>
+        <button className="boton-atras" onClick={handleAtras}>Atras</button>
+        <button className="boton-escanear" onClick={handleIniciarEscaneo}>Iniciar escaneo</button>
 
-          {saborSeleccionado && (
-            <div className="scaner-container"> 
-              <h2>Resultado del Escaneo:</h2>
-              <div className="resultado"><p >Producto: {saborSeleccionado.nombre}</p ></div>
-              <div className="resultado"> <p >Stock: {saborSeleccionado.stock}</p ></div>
-              <div className="resultado"><p >Umbral: {saborSeleccionado.umbral}</p ></div>
-            
-              <StockControl
-                accion={accion}
-                setAccion={setAccion}
-                cantidad={cantidad}
-                setCantidad={setCantidad}
-                handleActualizarStock={handleActualizarStock}
-              />
-            </div>
-          )}
+        {saborSeleccionado && (
+          <ManejoStock
+            saborSeleccionado={saborSeleccionado}
+            accion={accion}
+            setAccion={setAccion}
+            cantidad={cantidad}
+            setCantidad={setCantidad}
+            handleActualizarStock={handleActualizarStock}
+          />
+        )}
 
-           { !saborSeleccionado && (<div 
-            id="reader" 
-            style={{ width: "340px", height: "300px", marginTop: "7px" }}
-          ></div>)}
-       </div>   
+        {!saborSeleccionado && (
+          <div id="reader" style={{ width: "340px", height: "300px", marginTop: "7px" }}></div>
+        )}
+      </div>
     </div>
   );
 };
