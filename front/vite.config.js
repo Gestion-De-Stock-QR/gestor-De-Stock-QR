18b99822e5850119765fa6c7a08174a2ebfe3a87
diegoayza <diegoayza82@gmail.com>
2024-09-07 19:16:13 -0300

    cambiando la base del repo

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'https://gestion-de-stock-qr.github.io/gestor-De-Stock-QR/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
