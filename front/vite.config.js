cb86a2f474c798029424c4f61ead2f1271b24e96
diegoayza <diegoayza82@gmail.com>
2024-09-07 22:58:09 -0300

    Configuracion

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gestor-De-Stock-QR/', // nombre del repositorio dentro de la organización
   plugins: [react()],
-  base: 'https://gestion-de-stock-qr.github.io/gestor-De-Stock-QR/',
-
   server: {
     host: '0.0.0.0',
   },
