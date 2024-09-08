87bd830756990aa345221b4f42f90f0374a7f08f
francoleon42 <francoleonadress@gmail.com>
2024-09-08 10:07:44 -0300

    cambios desplegables

--- front/src/routes/routes.jsx
+++ front/src/routes/routes.jsx
@@ -1,40 +1,23 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../components/Home/Home";
 import { NuevoProducto } from "../components/pages/Producto/NuevoProducto";
 import { ActualizarStock } from "../components/pages/Producto/ActualizarStock";
 import { MisProductos } from "../components/pages/Producto/MisProductos";
 import { Historial } from "../components/pages/Producto/Historial";
 import Notificaciones from "../components/Home/Notificaciones";
-// import { Login } from '../components/pages/Login/Login'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/agregar-nuevo-producto",
-    element: <NuevoProducto />,
-  },
-  {
-    path: "/actualizar-stock",
-    element: <ActualizarStock />,
-  },
-  {
-    path: "/mis-productos",
-    element: <MisProductos />,
-  },
-  {
-    path: "/historial",
-    element: <Historial />,
-  },
-  {
-    path: "/notificaciones",
-    element: <Notificaciones/>
-  }
-]);
 
 export const MyRoutes = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Router basename="/gestor-De-Stock-QR"> {/* Asegúrate de que coincida con el nombre de tu repositorio */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agregar-nuevo-producto" element={<NuevoProducto />} />
+        <Route path="/actualizar-stock" element={<ActualizarStock />} />
+        <Route path="/mis-productos" element={<MisProductos />} />
+        <Route path="/historial" element={<Historial />} />
+        <Route path="/notificaciones" element={<Notificaciones />} />
+      </Routes>
+    </Router>
+  );
 };
--- front/vite.config.js
+++ front/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/gestor-De-Stock-QR/', 
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
+  
 });
