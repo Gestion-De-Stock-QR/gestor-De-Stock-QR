7d9db642fdab8d0ac700d87c35fb23ea27293957
diegoayza <diegoayza82@gmail.com>
2024-09-07 22:32:34 -0300

    cambio

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gestor-De-Stock-QR/',
   plugins: [react()],
+  base: '/gestor-De-Stock-QR/',
+
   server: {
     host: '0.0.0.0',
   },
